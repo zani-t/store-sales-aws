c0bb7557e3c31c3a51033d9361b04aaeae399416
Zani <tembo4.t@gmail.com>
2026-03-28 19:32:17 -0400

    storage stack implemented

--- app.py
+++ app.py
@@ -3,26 +3,19 @@ import os
 
 import aws_cdk as cdk
 
-from tsf2.tsf2_stack import Tsf2Stack
+from tsf2.storage_stack import StorageStack
 
 
 app = cdk.App()
-Tsf2Stack(app, "Tsf2Stack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
+env_name = app.node.try_get_context("env") or "dev"
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+StorageStack(
+    app, f"{env_name}-StorageStack",
+    env_name=env_name,
+    env=cdk.Environment(
+        account=os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region=os.getenv('CDK_DEFAULT_REGION')
+        )
     )
 
 app.synth()
---
+++ tsf2/storage_stack.py
@@ -0,0 +1,49 @@
+from aws_cdk import (
+    Stack,
+    aws_s3 as s3,
+    RemovalPolicy,
+    aws_dynamodb as dynamodb,
+)
+from constructs import Construct
+
+class StorageStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, env_name: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        removal = RemovalPolicy.RETAIN if env_name == "prod" else RemovalPolicy.DESTROY
+
+        # Data storage bucket
+        self.bucket = s3.Bucket(self, "Tsf2DataBucket",
+            versioned=True,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=removal,
+            auto_delete_objects=(env_name != "prod")
+        )
+
+        # Model storage bucket
+        self.model_bucket = s3.Bucket(self, "Tsf2ModelBucket",
+            versioned=True,
+            encryption=s3.BucketEncryption.S3_MANAGED,
+            block_public_access=s3.BlockPublicAccess.BLOCK_ALL,
+            removal_policy=removal,
+            auto_delete_objects=(env_name != "prod")
+        )
+    
+        # DynamoDB table for job metadata
+        self.job_table = dynamodb.Table(self, "Tsf2JobTable",
+            table_name=f"{env_name}-Tsf2JobMetadata",
+            partition_key=dynamodb.Attribute(name="job_id", type=dynamodb.AttributeType.STRING),
+            sort_key=dynamodb.Attribute(name="run_date", type=dynamodb.AttributeType.STRING),
+            removal_policy=removal,
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
+
+        # DynamoDB table for current model pointer
+        self.model_table = dynamodb.Table(self, "Tsf2ModelTable",
+            table_name="Tsf2ModelPointer",
+            partition_key=dynamodb.Attribute(name="model_id", type=dynamodb.AttributeType.STRING),
+            removal_policy=removal,
+            billing_mode=dynamodb.BillingMode.PAY_PER_REQUEST
+        )
